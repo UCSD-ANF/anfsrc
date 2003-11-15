59e9d8240e619a51c606b521c6b7bcd64d869ea9
Kent Lindquist <kent@lindquistconsulting.com>
2003-11-15 22:11:39 +0000

    Initial revision

---
+++ ext/wiggleview/RUNscripts/RUNAnza.bat
@@ -0,0 +1,3 @@
+set WIGGLEVIEW=C:\atul\download\wiggleview
+cd %WIGGLEVIEW%
+WIN32\Release\wiggleview data\Wiggleview.iv configFiles\StereoCameras.iv data\wilber\AnzaConfig.iv
---
+++ ext/wiggleview/RUNscripts/RUNTurkey.bat
@@ -0,0 +1,3 @@
+set WIGGLEVIEW=C:\atul\wiggleviewWilber
+cd %WIGGLEVIEW%
+WIN32\Release\wiggleview data\Wiggleview.iv configFiles\StereoCameras.iv data\wilber\TurkeyConfig.iv
