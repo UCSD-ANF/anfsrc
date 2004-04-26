e710ee44bee4fc1b4aefcf488b3944d4c9e9b50a
Kent Lindquist <kent@lindquistconsulting.com>
2004-04-26 21:44:05 +0000

    putArgsToString and getArgsToString changes

--- ext/srb/include/datascopeSrbTools.h
+++ ext/srb/include/datascopeSrbTools.h
@@ -1,12 +1,14 @@
 #ifndef DATASCOPE_SRB_TOOLS_H
 #define DATASCOPE_SRB_TOOLS_H
 
+#include <stdarg.h>
 #include "db.h"
 #include "stock.h"
 
 #define MAX_PROC_ARGS_FOR_DS 100
 
-extern int getArgsFromString(char *inStr, char *argv[], char del);
+extern char *putArgsToString(char del, char esc, int nargs, ...);
+extern int getArgsFromString(char *inStr, char *argv[], char del, char esc);
 extern int str2dbPtr(char * inBuf, Dbptr*   datascopedbPtr);
 extern int dbPtr2str(Dbptr* datascopedbPtr,  char *outBuf);
 extern int unescapeDelimiter(char *inOutStr, char del, char esc);
