eb793d93124e95b9aa8fcbb45eab3483364c05e0
Kent Lindquist <kent@lindquistconsulting.com>
2004-04-27 19:53:19 +0000

    dbclose,dbfind,dbfilename headers

--- ext/srb/utilities/include/dssrb.h
+++ ext/srb/utilities/include/dssrb.h
@@ -14,6 +14,9 @@ extern char mdasCollectionHome[];
 extern char inCondition[];
 
 extern int srb_dbopen( char *path, char *permissions, Dbptr *db );
+extern int srb_dbclose( Dbptr db );
 extern Dbptr srb_dblookup( Dbptr db, char *database_name, char *table_name, char *field_name, char *record_name );
+extern int srb_dbfind( Dbptr db, char *string, int flags, Hook **hook );
+extern int srb_dbfilename( Dbptr db, char *filename );
 
 #endif /* DSSRB_H */
