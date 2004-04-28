6738a2338424516d01da799ba1147c5a76e367e0
Kent Lindquist <kent@lindquistconsulting.com>
2004-04-28 23:18:34 +0000

    add str2dbTable, str2dbArray. Fix bugs in dbTbl2str and dbArr2str

--- ext/srb/include/datascopeSrbTools.h
+++ ext/srb/include/datascopeSrbTools.h
@@ -17,5 +17,7 @@ extern int unescapeDelimiter(char *inOutStr, char del, char esc);
 extern int escapeDelimiter(char *inStr, char *outStr, char del, char esc);
 extern int dbTable2str(Tbl *inTbl, char *outStr);
 extern int dbArray2str(Arr *inArr, char *outStr);
+extern Tbl *str2dbTable(char *inStr);
+extern Arr *str2dbArray(char *inStr);
 
 #endif /* DATASCOPE_SRB_TOOLS_H */
