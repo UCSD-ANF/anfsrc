26f9c79801b3e23617d7c31ac635174e757b233e
Kent Lindquist <kent@lindquistconsulting.com>
2005-02-12 00:06:41 +0000

    Remove our early efforts to make Antelope-linked wiggleview rt

--- ext/wiggleview/RUNscripts/RUNAnza.bat
+++
@@ -1,3 +0,0 @@
-set WIGGLEVIEW=C:\atul\download\wiggleview
-cd %WIGGLEVIEW%
-WIN32\Release\wiggleview data\Wiggleview.iv configFiles\StereoCameras.iv data\wilber\AnzaConfig.iv
--- ext/wiggleview/RUNscripts/RUNTurkey.bat
+++
@@ -1,3 +0,0 @@
-set WIGGLEVIEW=C:\atul\wiggleviewWilber
-cd %WIGGLEVIEW%
-WIN32\Release\wiggleview data\Wiggleview.iv configFiles\StereoCameras.iv data\wilber\TurkeyConfig.iv
