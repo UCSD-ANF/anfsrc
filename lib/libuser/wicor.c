32b9a8fcac341d10d14bf47c289e5f4b0add2974
ROADNet Staff <roadnet-orb-admins@epicenter.ucsd.edu>
2007-01-08 18:44:22 +0000

    Changed Revelle sample rate to 1, left others (melville) as it was

--- lib/libuser/wicor.c
+++ lib/libuser/wicor.c
@@ -74,7 +74,11 @@ unstuff_wicor (char *srcname, double ipkttime, char *packet, int nbytes, Packet 
       channel=newPktChannel();
       channel->data=malloc(sizeof(int));
       channel->time=pkt->time;
-      channel->samprate=0.067;/*66666;*/
+      if (strncmp(srcparts.src_sta,"RevelleMET",10) == 0) {
+         channel->samprate=1;
+      } else {
+         channel->samprate=0.067;/*66666;*/
+      }
       channel->calper=-1;
       channel->nsamp=1;
       channel->datasz=1;
