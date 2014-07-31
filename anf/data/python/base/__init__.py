e37b0d8ae4bbc1bfcffffc7a67e835339ec357c8
Geoff Davis <gadavis@ucsd.edu>
2014-07-30 18:54:13 -0700

    Fix stock import

--- anf/data/python/base/__init__.py
+++ anf/data/python/base/__init__.py
@@ -2,6 +2,7 @@
 ANF functions for Antelope
 """
 
+import antelope
 def deep_auto_convert(data):
     """
     call stock.auto_convert on all entries in a dict or array.
@@ -18,7 +19,7 @@ def deep_auto_convert(data):
         for i in range(len(data)):
             data[i] = deep_auto_convert(data[i])
     elif type(data) is str:
-        data = stock.auto_convert(data)
+        data = antelope.stock.auto_convert(data)
     else:
         pass # no-op on unrecognized items
 
