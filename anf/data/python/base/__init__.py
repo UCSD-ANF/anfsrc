43fdc2a23067c9574161df162b6b81b00d2ed853
Geoff Davis <gadavis@ucsd.edu>
2014-07-30 18:39:32 -0700

    add anf.deep_auto_convert function

--- anf/data/python/base/__init__.py
+++ anf/data/python/base/__init__.py
@@ -2,4 +2,26 @@
 ANF functions for Antelope
 """
 
+def deep_auto_convert(data):
+    """
+    call stock.auto_convert on all entries in a dict or array.
+
+    the stock.ParameterFile.get() routine in the Antelope 5.3+ python bindings
+    calls stock.auto_convert, but that routine doesn't recursively auto-convert
+    all of the entries in a pf Arr object or a Tbl object.
+    """
+
+    if type(data) is dict:
+        for i in data.keys():
+            data[i] = deep_auto_convert(data[i])
+    elif type(data) is list:
+        for i in range(len(data)):
+            data[i] = deep_auto_convert(data[i])
+    elif type(data) is str:
+        data = stock.auto_convert(data)
+    else:
+        pass # no-op on unrecognized items
+
+    return data
+
 pass
