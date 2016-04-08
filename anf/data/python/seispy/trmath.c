c7436014bab47eb11463be0d9cc5ac73ac7e20da
Malcolm White <mcwhite@ucsd.edu>
2016-04-08 15:49:47 -0700

    Add function to cast floating-point trace-data to integers, in place.

---
+++ anf/data/python/seispy/trmath.c
@@ -0,0 +1,39 @@
+#include <Python.h>
+#include <math.h>
+
+static PyObject *
+trmath_tr_float_to_int(PyObject *self, PyObject *args)
+{
+    PyObject* tr;
+    PyObject* lsc;
+    PyObject* temp;
+    int trl;
+    int i;
+
+    if (!PyArg_ParseTuple(args, "O!O!", &PyList_Type, &tr,
+                                        &PyFloat_Type, &lsc))
+        return NULL;
+
+    trl = PyList_Size(tr);
+
+    for (i = 0; i < trl; i++ ){
+        temp = PyNumber_TrueDivide(PyList_GetItem(tr, i), lsc);
+        PyList_SetItem(tr, i, PyInt_FromLong((long) round(PyFloat_AsDouble(temp))));
+        Py_DECREF(temp);
+    }
+
+    return Py_BuildValue("i", 1);
+
+}
+
+static PyMethodDef TrMathMethods[] = {
+    {"tr_float_to_int", trmath_tr_float_to_int, METH_VARARGS,
+     "Cast floating point trace data to integer counts given the least-significant count value."},
+    {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+inittrmath(void)
+{
+    (void) Py_InitModule("trmath", TrMathMethods);
+}
