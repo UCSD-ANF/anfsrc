db0968fa24ddacb80e4e2e21d8df2cae1c2e7f78
Geoff Davis <gadavis@ucsd.edu>
2020-01-10 16:54:57 -0800

    Python3 fixes

--- anf/bin/utility/rocket_impacts/rocket_impacts.py
+++ anf/bin/utility/rocket_impacts/rocket_impacts.py
@@ -4,26 +4,23 @@
 # can input in this tool to plot the data:
 #   https://www.freemaptools.com/concentric-circles.htm
 
-import os
-import sys
 import json
 
-print "Locate Rocket impact sites:"
-print "Copy/Paste the text to this website:"
-print "\thttps://www.freemaptools.com/concentric-circles.htm"
-print "\n"
+print ("Locate Rocket impact sites: \n")
+print ("Copy/Paste the text to this website:")
+print ("\thttps://www.freemaptools.com/concentric-circles.htm\n")
 
 
 sites = json.load(open("rocket_impacts_sites.conf"))
 
 # plot sites
 for sta in sites:
-    print '"%f,%f",0,3,%s' % (sites[sta]['lat'], sites[sta]['lon'], sites[sta]['color'])
+    print('"%f,%f",0,3,%s' % (sites[sta]['lat'], sites[sta]['lon'], sites[sta]['color']))
 
 # plot known points
 points = json.load(open("rocket_impacts_points.conf"))
 for point in points:
-    print '"%f,%f",0,%d,%s' % (point[0],point[1], point[2], point[3])
+    print('"%f,%f",0,%d,%s' % (point[0],point[1], point[2], point[3]))
 
 # plot circles
 times = [90,105]
@@ -47,9 +44,9 @@ for k in delay:
         start = ( ( times[0] + time_delay ) * speed ) / m2km
         end = ( ( times[1] + time_delay ) * speed ) / m2km
 
-        print '"%f,%f",%f,%f,%s' % \
+        print('"%f,%f",%f,%f,%s' % \
                 (sites[k]['lat'], sites[k]['lon'],
-                start, end, sites[k]['color'])
+                start, end, sites[k]['color']))
 
     else:
-        print "SITE %s NOT IN CONFIG FILE" % k
+        print("SITE %s NOT IN CONFIG FILE" % k)
