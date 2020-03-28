592778171e2fa4efd8023034c168f9d463461d25
Geoff Davis <gadavis@ucsd.edu>
2020-03-28 09:12:16 -0700

    Make trmath compile under py2 and 3
    Bunch more internal changes needed to make the module compile under both
    python 2 and 3. Code borrowed from:
     https://docs.python.org/3.6/howto/cporting.html#module-initialization-and-state

--- anf/data/python/seispy/trmath.c
+++ anf/data/python/seispy/trmath.c
@@ -7,6 +7,24 @@
 
 #include <math.h>
 
+struct module_state {
+    PyObject *error;
+};
+
+#if PY_MAJOR_VERSION >= 3
+#define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
+#else
+#define GETSTATE(m) (&_state)
+static struct module_state _state;
+#endif
+
+static PyObject *
+error_out(PyObject *m) {
+    struct module_state *st = GETSTATE(m);
+    PyErr_SetString(st->error, "something bad happened");
+    return NULL;
+}
+
 static PyObject *
 trmath_tr_float_to_int(PyObject *self, PyObject *args)
 {
@@ -32,14 +50,63 @@ trmath_tr_float_to_int(PyObject *self, PyObject *args)
 
 }
 
-static PyMethodDef TrMathMethods[] = {
+static PyMethodDef trmath_methods[] = {
     {"tr_float_to_int", trmath_tr_float_to_int, METH_VARARGS,
      "Cast floating point trace data to integer counts given the least-significant count value."},
     {NULL, NULL, 0, NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
+static int trmath_traverse(PyObject *m, visitproc visit, void *arg) {
+    Py_VISIT(GETSTATE(m)->error);
+    return 0;
+}
+
+static int trmath_clear(PyObject *m) {
+    Py_CLEAR(GETSTATE(m)->error);
+    return 0;
+}
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "trmath",
+        NULL,
+        sizeof(struct module_state),
+        trmath_methods,
+        NULL,
+        trmath_traverse,
+        trmath_clear,
+        NULL
+};
+
+#define INITERROR return NULL
+
 PyMODINIT_FUNC
+PyInit_trmath(void)
+#else
+#define INITERROR return
+
+void
 inittrmath(void)
+#endif
 {
-    (void) Py_InitModule("trmath", TrMathMethods);
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&moduledef);
+#else
+    PyObject *module = Py_InitModule("trmath", trmath_methods);
+#endif
+
+    if (module == NULL)
+        INITERROR;
+    struct module_state *st = GETSTATE(module);
+
+    st->error = PyErr_NewException("trmath.Error", NULL, NULL);
+    if (st->error == NULL) {
+        Py_DECREF(module);
+        INITERROR;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    return module;
+#endif
 }
